5fcf236ca1dd5fcfe8ff814d0226f49248318c39
PriyangaPKini <priyangapkini99@gmail.com>
2020-02-01 15:55:12 +0530

    Created an end point

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -5,6 +5,8 @@ using Domain;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Persistance;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace API.Controllers
 {    
@@ -13,15 +15,22 @@ namespace API.Controllers
     public class ActivitiesController : ControllerBase
     {
         private readonly IMediator _mediator;
+
         public ActivitiesController(IMediator mediator)
         {
-            _mediator = mediator;
+            _mediator = mediator;    
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> List()
         {
             return await _mediator.Send(new List.Query());   
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> Details(Guid id)
+        {g  
+            return await _mediator.Send(new Details.Query{Id = id});
         }
     }
 
