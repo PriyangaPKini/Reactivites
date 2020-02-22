7377978e6cdfdaff99086f3f96b46ef3f46b9567
PriyangaPKini <priyangapkini99@gmail.com>
2020-02-22 12:50:04 +0530

    Listing view of activities

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,32 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment} from "react";
 //import { render } from '@testing-library/react';
-import axios from 'axios';
-import { Header, Icon, List } from 'semantic-ui-react';
-import { IActivity } from '../models/activity';
+import axios from "axios";
+import { Header, Icon, List, Container } from "semantic-ui-react";
+import { IActivity } from "../models/activity";
+import { NavBar } from "../../features/nav/NavBar";
+import { ActivityDashbord } from "../../features/activities/dashbord/ActivityDashbord";
 
-const  App = () => {
-  const [activities, setActivities] = useState<IActivity[]>([])
-  useEffect(() =>{
-   axios.get<IActivity[]>('http://localhost:5000/api/activities')
-        .then((response) => {
-         setActivities(response.data)
-        });
-  }, []); 
+const App = () => {
+  const [activities, setActivities] = useState<IActivity[]>([]);
+  useEffect(() => {
+    axios
+      .get<IActivity[]>("http://localhost:5000/api/activities")
+      .then(response => {
+        setActivities(response.data);
+      });
+  }, []);
   return (
-    <div >
-        <Header as='h2'>
-            <Icon name='users' />
-            <Header.Content>Reactivities</Header.Content>
-        </Header>
-        <List> 
-          {activities.map((activity)=>
-          (
-            <List.Item key = {activity.id}>{activity.title}</List.Item>
-          ))}      
-        </List>    
-    </div>
+    <Fragment>
+      <NavBar/>
+      <Container style={{marginTop: '7em'}}>
+        <ActivityDashbord activities = {activities}/>
+      </Container>
+    </Fragment>
   );
-}
+};
 
 export default App;
-
