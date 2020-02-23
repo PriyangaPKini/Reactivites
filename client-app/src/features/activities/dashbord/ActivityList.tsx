8c369d7f1d0624032291e447e7d7215308f65e14
PriyangaPKini <priyangapkini99@gmail.com>
2020-02-24 02:57:02 +0530

    added crud feature in client side

--- client-app/src/features/activities/dashbord/ActivityList.tsx
+++ client-app/src/features/activities/dashbord/ActivityList.tsx
@@ -4,9 +4,15 @@ import { IActivity } from "../../../app/models/activity";
 
 interface IProps {
   activities: IActivity[];
+  selectActivity: (id: string) => void;
+  deleteActivity: (id:string) => void;
 }
 
-export const ActivityList: React.FC<IProps> = ({ activities }) => {
+export const ActivityList: React.FC<IProps> = ({
+  activities,
+  selectActivity,
+  deleteActivity
+}) => {
   return (
     <Segment clearing>
       <Item.Group divided>
@@ -17,11 +23,24 @@ export const ActivityList: React.FC<IProps> = ({ activities }) => {
               <Item.Meta>{activity.date}</Item.Meta>
               <Item.Description>
                 <div>{activity.description}</div>
-                <div>{activity.city}, {activity.venue}</div>
+                <div>
+                  {activity.city}, {activity.venue}
+                </div>
               </Item.Description>
               <Item.Extra>
-                <Button floated="right" content="view" color="blue"></Button>
-                <Label basic content= {activity.category} />
+                <Button
+                  onClick={() => selectActivity(activity.id)}
+                  floated="right"
+                  content="View"
+                  color="blue"
+                ></Button>
+                <Button
+                  onClick={() => deleteActivity(activity.id)}
+                  floated="right"
+                  content="Delete"
+                  color="red"
+                ></Button>
+                <Label basic content={activity.category} />
               </Item.Extra>
             </Item.Content>
           </Item>
